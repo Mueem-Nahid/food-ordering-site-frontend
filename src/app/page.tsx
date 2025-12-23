536dc97de99862abadd1b9430279b07aa79f21fe
Mueem-Nahid <mueem51@gmail.com>
2025-12-23 21:59:51 +0600

    # Info - clear all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import Hero from "@/components/Home/Hero";
-import Features from "@/components/Home/Features";
-import Cook from "@/components/Home/Cook";
-import Expert from "@/components/Home/Expert";
-import Gallery from "@/components/Home/Gallery";
-import Newsletter from "@/components/Home/Newsletter";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "DeshiQ.",
@@ -13,12 +7,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Gallery />
-      <Features />
-      <Cook />
-      <Expert />
-      <Newsletter />
+
     </main>
   );
 }

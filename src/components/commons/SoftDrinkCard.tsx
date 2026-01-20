f92662aac20da6dfdd164b24cac4193c61a1f5ed
Mueem-Nahid <mueem51@gmail.com>
2026-01-20 14:26:30 +0600

    # Info - not available addon and drinks

--- src/components/commons/SoftDrinkCard.tsx
+++ src/components/commons/SoftDrinkCard.tsx
@@ -18,42 +18,42 @@ interface SoftDrink {
   [key: string]: any;
 }
 
-const softDrinksDummy = [
-  {
-    "_id": "62f95d3c0ecf5ffea243c9d9",
-    "name": "7UP Diet",
-    "price": 100,
-    "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509500/kfc-clone/vbbypmkpufrtgdtnd7pu.png",
-    "__v": 0
-  },
-  {
-    "_id": "62f95d660ecf5ffea243c9db",
-    "name": "Mirinda Regular",
-    "price": 100,
-    "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509541/kfc-clone/e4w3leiiw81soldkweno.png",
-    "__v": 0
-  },
-  {
-    "_id": "62f95da50ecf5ffea243c9dd",
-    "name": "Mountain Dew Regular",
-    "price": 100,
-    "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509605/kfc-clone/kqolpdgsvv1mtfsqplaa.png",
-    "__v": 0
-  },
-  {
-    "_id": "62f95f150ecf5ffea243c9df",
-    "name": "Pepsi Diet Regular",
-    "price": 100,
-    "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509973/kfc-clone/yiekbllnfoncwbnsypqt.png",
-    "__v": 0
-  },
-  {
-    "_id": "62f95f220ecf5ffea243c9e1",
-    "name": "Pepsi Regular",
-    "price": 100,
-    "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509986/kfc-clone/tynas6xbflqqosbdqbf0.png",
-    "__v": 0
-  }
+const softDrinksDummy: [] = [
+  // {
+  //   "_id": "62f95d3c0ecf5ffea243c9d9",
+  //   "name": "7UP Diet",
+  //   "price": 100,
+  //   "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509500/kfc-clone/vbbypmkpufrtgdtnd7pu.png",
+  //   "__v": 0
+  // },
+  // {
+  //   "_id": "62f95d660ecf5ffea243c9db",
+  //   "name": "Mirinda Regular",
+  //   "price": 100,
+  //   "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509541/kfc-clone/e4w3leiiw81soldkweno.png",
+  //   "__v": 0
+  // },
+  // {
+  //   "_id": "62f95da50ecf5ffea243c9dd",
+  //   "name": "Mountain Dew Regular",
+  //   "price": 100,
+  //   "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509605/kfc-clone/kqolpdgsvv1mtfsqplaa.png",
+  //   "__v": 0
+  // },
+  // {
+  //   "_id": "62f95f150ecf5ffea243c9df",
+  //   "name": "Pepsi Diet Regular",
+  //   "price": 100,
+  //   "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509973/kfc-clone/yiekbllnfoncwbnsypqt.png",
+  //   "__v": 0
+  // },
+  // {
+  //   "_id": "62f95f220ecf5ffea243c9e1",
+  //   "name": "Pepsi Regular",
+  //   "price": 100,
+  //   "pic": "https://res.cloudinary.com/digaxe3sc/image/upload/v1660509986/kfc-clone/tynas6xbflqqosbdqbf0.png",
+  //   "__v": 0
+  // }
 ]
 
 const SoftDrinkCard: React.FC<SoftDrinkCardProps> = ({ title, prod_id }) => {
@@ -89,6 +89,10 @@ const SoftDrinkCard: React.FC<SoftDrinkCardProps> = ({ title, prod_id }) => {
         <span className="optional">{t("optional")}</span>
       </div>
       <div className="addon-item">
+        {
+          softDrinksDummy.length === 0 &&
+          <div>Not available at the moment.</div>
+        }
         {softDrinksDummy.slice(0, 4).map((softDrink: SoftDrink, index: number) => {
           return (
             <div className="addon-info" key={index}>
